0e38f4f7375addf34b5389d5911cf15bd2478480
Takuya Narihira <dvbscb@gmail.com>
2015-02-03 09:47:43 -0800

    Added caffe utilities, train_by_net etc.
    methods : train_by_net, get_solver_prototxt, get_output
    jinja2 filters : max, min, slice_list

--- python/caffe_helper/tools.py
+++ python/caffe_helper/tools.py
@@ -1,4 +1,5 @@
 import os
+import glob
 import sys
 import datetime
 from subprocess import call
@@ -7,6 +8,17 @@ pj = os.path.join
 
 import numpy as np
 
+def j2filter_slice_list(value, slices):
+    try:
+        return [value[i] for i in slices]
+    except TypeError:
+        return value[:slices]
+
+J2FILTERS = {
+    'max': max,
+    'min': min,
+    'slice_list': j2filter_slice_list,
+}
 
 class CaffeHelper(object):
 
@@ -38,9 +50,11 @@ class CaffeHelper(object):
         )
         from jinja2 import Environment, FileSystemLoader
         env = Environment()
+        env.filters.update(J2FILTERS)
         env.loader = FileSystemLoader(path_jinja2)
         self.j2env_ = env
         self.dir_proto_out_ = pj(self.caffe_root_, 'tmp', 'prototxt')
+        self.dir_template_ = path_jinja2
         if not os.path.isdir(self.dir_proto_out_):
             os.makedirs(self.dir_proto_out_)
 
@@ -72,6 +86,7 @@ class CaffeHelper(object):
             tokens += ['-weights', pretrained]
         if snapshot is not None:
             tokens += ['-snapshot', snapshot]
+        tokens = map(str, tokens)
         now = datetime.datetime.now().strftime('%y%m%d-%H%M%S')
         logname = 'train.%s.%s' % (now, os.path.basename(solver_text))
         fo = pj(self.dir_log_, logname + '.o')
@@ -85,7 +100,8 @@ class CaffeHelper(object):
             print '####### stderr'
             call(['tail', fe])
 
-    def convert_prototxt_template(self, path_template, path_proto=None, **proto_kw):
+    def convert_prototxt_template(self, path_template, path_proto=None,
+        **proto_kw):
         """
         """
         if path_proto is None:
@@ -97,6 +113,74 @@ class CaffeHelper(object):
             tmpl = self.j2env_.from_string(open(path_template).read())
             print >> fd, tmpl.render(**proto_kw)
         return path_proto
+
+    def get_solver_prototxt(self, net,
+        snapshot_prefix, max_iter, test_iter=200,
+        test_interval=100, base_lr=0.001, display=50, momentum=0.9,
+        weight_decay=1e-6, snapshot=100, debug_info=True):
+        """
+        """
+        kw = locals().copy()
+        del kw['self']
+        path_solver = self.convert_prototxt_template(
+            pj(self.dir_template_, 'solver.prototxt.jinja2'), **kw)
+        return path_solver
+
+    def train_by_net(self, net,
+        snapshot_prefix, max_iter, test_iter=200,
+        test_interval=100, base_lr=0.001, display=50, momentum=0.9,
+        weight_decay=1e-6, snapshot=100, debug_info=True,
+        gpu=-1, path_pretrained=None, path_snapshot=None,
+        dry_run=False):
+        """
+        """
+        kw = locals().copy()
+        del kw['self']
+        del kw['gpu']
+        del kw['path_pretrained']
+        del kw['path_snapshot']
+        del kw['dry_run']
+        path_solver = self.get_solver_prototxt(**kw)
+        if not dry_run:
+            self.train(path_solver, gpu=gpu,
+                pretrained=path_pretrained, snapshot=path_snapshot)
+        key = lambda x: int(x.split('_')[-1].split('.')[0])
+        models = filter(lambda x: key(x) <= max_iter,
+            sorted(glob.glob(snapshot_prefix + "_iter_*.caffemodel"),
+                key=key))
+        snapshots = filter(lambda x: key(x) <= max_iter,
+            sorted(glob.glob(snapshot_prefix + "_iter_*.solverstate"),
+                key=key))
+        return models, snapshots
+
+    def get_output(self, blobs, proto_path, model_path,
+        num_sample=173519, gpu=0):
+        import caffe
+        if gpu < 0:
+            caffe.set_mode_cpu()
+        else:
+            caffe.set_mode_gpu()
+            caffe.set_device(gpu)
+        caffe.set_phase_test()
+        net = caffe.Net(proto_path, model_path)
+        num_iter = -1
+        batch_size = -1
+        i = 0
+        outputs = dict(zip(blobs, [[] for i in xrange(len(blobs))]))
+        while True:
+            ret = net.forward(blobs=blobs)
+            if num_iter < 0:
+                batch_size = ret[blobs[0]].shape[0]
+                num_iter = int(np.ceil(num_sample * 1.0 / batch_size))
+            i += 1
+            if num_iter == i:
+                for b in blobs:
+                    outputs[b] += [ret[b][:num_sample%batch_size].copy()]
+                    outputs[b] = np.concatenate(outputs[b], axis=0)
+                break
+            for b in blobs:
+                outputs[b] += [ret[b].copy()]
+        return outputs
 
     def draw_net_from_prototxt(self, path_proto, rankdir='LR', **proto_kw):
         """
