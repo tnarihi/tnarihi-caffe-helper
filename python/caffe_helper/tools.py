0da0e4e7c773386a63b22526efa9f67d55e64e30
Takuya Narihira <dvbscb@gmail.com>
2015-02-04 10:05:28 -0800

    Add method to retrieve models and solverstates
    Calling system function by Popen so that we can terminate the child
    processes when parent is terminated.

--- python/caffe_helper/tools.py
+++ python/caffe_helper/tools.py
@@ -2,11 +2,12 @@ import os
 import glob
 import sys
 import datetime
-from subprocess import call
+from subprocess import Popen
 import hashlib
 pj = os.path.join
 
 import numpy as np
+
 
 def j2filter_slice_list(value, slices):
     try:
@@ -19,6 +20,25 @@ J2FILTERS = {
     'min': min,
     'slice_list': j2filter_slice_list,
 }
+
+
+def call_subprocess(*args, **kw):
+    try:
+        p = Popen(*args, **kw)
+        p.communicate()
+        ret = p.returncode
+        if ret != 0:
+            raise RuntimeError(
+                'Process "%s" exit with status %d' % (str(args[0]), ret))
+    except:
+        """This doen't ensure that child processes is killed when parent
+        crashes"""
+        if p.returncode is None:
+            p.terminate()
+            p.kill()
+            print >> sys.stderr, 'Process killed: pid=%d command="%s"' % (
+                p.pid, str(args[0]))
+
 
 class CaffeHelper(object):
 
@@ -93,15 +113,15 @@ class CaffeHelper(object):
         fe = pj(self.dir_log_, logname + '.e')
         if self.verbose_:
             print 'run "%s"' % ' '.join(tokens)
-        call(tokens, stdout=open(fo, 'w'), stderr=open(fe, 'w'))
+        call_subprocess(tokens, stdout=open(fo, 'w'), stderr=open(fe, 'w'))
         if self.verbose_:
             print '####### stdout'
-            call(['tail', fo])
+            call_subprocess(['tail', fo])
             print '####### stderr'
-            call(['tail', fe])
+            call_subprocess(['tail', fe])
 
     def convert_prototxt_template(self, path_template, path_proto=None,
-        **proto_kw):
+                                  **proto_kw):
         """
         """
         if path_proto is None:
@@ -115,9 +135,10 @@ class CaffeHelper(object):
         return path_proto
 
     def get_solver_prototxt(self, net,
-        snapshot_prefix, max_iter, test_iter=200,
-        test_interval=100, base_lr=0.001, display=50, momentum=0.9,
-        weight_decay=1e-6, snapshot=100, debug_info=True):
+                            snapshot_prefix, max_iter, test_iter=200,
+                            test_interval=100, base_lr=0.001,
+                            display=50, momentum=0.9,
+                            weight_decay=1e-6, snapshot=100, debug_info=True):
         """
         """
         kw = locals().copy()
@@ -126,12 +147,28 @@ class CaffeHelper(object):
             pj(self.dir_template_, 'solver.prototxt.jinja2'), **kw)
         return path_solver
 
+    @staticmethod
+    def get_iter_from_path(pathi):
+        return int(pathi.split('_')[-1].split('.')[0])
+
+    @staticmethod
+    def get_models_and_states_from_prefix(snapshot_prefix):
+        models = sorted(
+            glob.glob(snapshot_prefix + "_iter_*.caffemodel"),
+            key=CaffeHelper.get_iter_from_path)
+        states = sorted(glob.glob(snapshot_prefix + "_iter_*.solverstate"),
+                        key=CaffeHelper.get_iter_from_path)
+        assert len(models) == len(states)
+        iters = map(CaffeHelper.get_iter_from_path, models)
+        return list(zip(iters, models, states))
+
     def train_by_net(self, net,
-        snapshot_prefix, max_iter, test_iter=200,
-        test_interval=100, base_lr=0.001, display=50, momentum=0.9,
-        weight_decay=1e-6, snapshot=100, debug_info=True,
-        gpu=-1, path_pretrained=None, path_snapshot=None,
-        dry_run=False):
+                     snapshot_prefix, max_iter, test_iter=200,
+                     test_interval=100, base_lr=0.001,
+                     display=50, momentum=0.9,
+                     weight_decay=1e-6, snapshot=100, debug_info=True,
+                     gpu=-1, path_pretrained=None, path_snapshot=None,
+                     dry_run=False):
         """
         """
         kw = locals().copy()
@@ -143,18 +180,11 @@ class CaffeHelper(object):
         path_solver = self.get_solver_prototxt(**kw)
         if not dry_run:
             self.train(path_solver, gpu=gpu,
-                pretrained=path_pretrained, snapshot=path_snapshot)
-        key = lambda x: int(x.split('_')[-1].split('.')[0])
-        models = filter(lambda x: key(x) <= max_iter,
-            sorted(glob.glob(snapshot_prefix + "_iter_*.caffemodel"),
-                key=key))
-        snapshots = filter(lambda x: key(x) <= max_iter,
-            sorted(glob.glob(snapshot_prefix + "_iter_*.solverstate"),
-                key=key))
-        return models, snapshots
+                       pretrained=path_pretrained, snapshot=path_snapshot)
+        return self.get_models_and_states_from_prefix(snapshot_prefix)
 
     def get_output(self, blobs, proto_path, model_path,
-        num_sample=173519, gpu=0):
+                   num_sample=173519, gpu=0):
         import caffe
         if gpu < 0:
             caffe.set_mode_cpu()
@@ -175,7 +205,7 @@ class CaffeHelper(object):
             i += 1
             if num_iter == i:
                 for b in blobs:
-                    outputs[b] += [ret[b][:num_sample%batch_size].copy()]
+                    outputs[b] += [ret[b][:num_sample % batch_size].copy()]
                     outputs[b] = np.concatenate(outputs[b], axis=0)
                 break
             for b in blobs:
